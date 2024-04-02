e0ff3352de32f1a671972256cac1f85f9ef9ebb9
Bruno Oliveira <86791361+Bruno514@users.noreply.github.com>
2024-04-02 16:01:16 -0300

    initial commit

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import { Node } from "../lib/node.js";
+import { LinkedList } from "../lib/linkedList.js";
+
+const node = new Node("teste");
+const list = new LinkedList(node);
+list.pop();
+console.log(list.toString());
+
+list.append(new Node(32));
+list.append(new Node(64));
+
+console.log(list.size());
+
+list.prepend(new Node("Sfasdf"));
+list.prepend(new Node("Sfasdf"));
+
+console.log(list.size());
+list.pop();
+console.log(list.toString());
+console.log(list.contains(32));
+console.log(list.find(32));
+list.insertAt(342, 2);
+console.log(list.toString());
+
+
+list.removeAt(0);
+console.log(list.toString())
